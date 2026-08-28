5ead79f83274ff1b4b2d122ac359d573a3adafe9
seungyunpark <115516297+sypark1995@users.noreply.github.com>
2026-08-28 19:36:50 +0900

    feat: 특가 피드를 Travelpayouts 실데이터로 전환

--- data/src/main/java/com/sypark/flightdeal/data/di/RepositoryModule.kt
+++ data/src/main/java/com/sypark/flightdeal/data/di/RepositoryModule.kt
@@ -1,16 +1,19 @@
 package com.sypark.flightdeal.data.di
 
-import com.sypark.flightdeal.data.fake.FakeFlightPriceRepository
+import com.sypark.flightdeal.data.BuildConfig
+import com.sypark.flightdeal.data.remote.TravelpayoutsApi
+import com.sypark.flightdeal.data.remote.TravelpayoutsFlightPriceRepository
 import com.sypark.flightdeal.domain.repository.FlightPriceRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import java.time.Clock
 import javax.inject.Singleton
 
 /**
- * 지금은 Fake만 제공한다. Travelpayouts 구현체가 생기면
- * 이 함수의 반환값만 교체한다. 다른 어떤 파일도 손대지 않는다.
+ * Fake는 지우지 않는다. 테스트와 오프라인 개발에 계속 쓴다.
+ * 여기 반환값만 바꾸면 앱 전체가 Fake와 실데이터 사이를 오간다.
  */
 @Module
 @InstallIn(SingletonComponent::class)
@@ -18,6 +21,10 @@ object RepositoryModule {
 
     @Provides
     @Singleton
-    fun provideFlightPriceRepository(): FlightPriceRepository =
-        FakeFlightPriceRepository()
+    fun provideFlightPriceRepository(api: TravelpayoutsApi): FlightPriceRepository =
+        TravelpayoutsFlightPriceRepository(
+            api = api,
+            marker = BuildConfig.TRAVELPAYOUTS_MARKER,
+            clock = Clock.systemDefaultZone(),
+        )
 }
